e4679ae558ba894c376c33e8663f5b1677610652
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-12-29 17:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // Remove color after 1 second
       setTimeout(() => {
-        square.style.backgroundColor = rgb(221, 221, 221);  // Default color after hover
+        square.style.backgroundColor = rgb(29, 29, 29);  // Default color after hover
       }, 1000);
     });
   }
